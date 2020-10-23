e8e6e6e286d6862488c24e37bc7a4432048decb3
smileyface <kasonbennett65@gmail.com>
2020-10-23 11:04:51 -0700

    Desktop interface generalzation

--- dev/View/Windows/view/devices/led_view.h
+++
@@ -1,9 +0,0 @@
-#include "../view.h"
-
-class LED_View : public View
-{
-	LED_View(const char CLASS_NAME[], const char WIN_TEXT[], int nCmdShow)
-	{
-		initalize(CLASS_NAME, WIN_TEXT, nCmdShow);
-	};
-};
--- dev/View/Windows/views.h
+++
@@ -1,3 +0,0 @@
-#include "view/view.h"
-#include "view/devices/main_view.h"
-#include "view/devices/led_view.h"
