c21706c860dd4f545e667d15286fa9cc9d18ec90
smileyface <kasonbennett65@gmail.com>
2020-10-23 11:04:51 -0700

    Desktop interface generalzation

--- dev/View/Desktop/Interfaces/Windows/main.cpp
+++ dev/View/Desktop/Interfaces/Windows/main.cpp
@@ -1,12 +1,12 @@
 
 #include <windows.h>
-#include "window.h"
+#include "../../desktop_main.h"
 
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
 
-    CC_Window_Tree_Interface::initalize(hInstance);
+    CC_Window_Tree_Interface::initalize();
     CC_Window_Tree_Interface::show_all();
     // Run the message loop.
 
---
+++ dev/View/Desktop/Interfaces/Windows/view/view_interface.h
@@ -0,0 +1,11 @@
+#ifndef VIEW_INTERFACE_PROC_H
+#define VIEW_INTERFACE_PROC_H
+
+
+
+#include <Windows.h>
+
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+#endif // !VIEW_INTERFACE_PROC_H
