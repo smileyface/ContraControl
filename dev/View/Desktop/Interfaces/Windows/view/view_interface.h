3f9938b223bbf495bba7ebb42beb4acf459ddcff
smileyface <kasonbennett65@gmail.com>
2020-10-23 15:21:01 -0700

    system guards

--- dev/View/Desktop/Interfaces/Windows/view/view_interface.h
+++ dev/View/Desktop/Interfaces/Windows/view/view_interface.h
@@ -1,11 +1,17 @@
 #ifndef VIEW_INTERFACE_PROC_H
 #define VIEW_INTERFACE_PROC_H
 
+#include "../../system_interface.h"
+#ifdef WIN32
+
+
 
 
 #include <Windows.h>
 
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+#endif // WIN32
 
 #endif // !VIEW_INTERFACE_PROC_H
