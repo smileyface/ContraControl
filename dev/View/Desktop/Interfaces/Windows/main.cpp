66af5eff07ca6a3297b726149bf7657361ff89c1
smileyface <kasonbennett65@gmail.com>
2020-10-29 09:40:41 -0700

    Fix null pointer issue

--- dev/View/Desktop/Interfaces/Windows/main.cpp
+++ dev/View/Desktop/Interfaces/Windows/main.cpp
@@ -1,11 +1,12 @@
 
 #include <windows.h>
 #include "../../desktop_main.h"
+#include "view/view_interface.h"
 
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-
+    window_hInstance = hInstance;
     CC_Window_Tree_Interface::initalize();
     CC_Window_Tree_Interface::show_all();
     // Run the message loop.
--- dev/View/Desktop/Interfaces/Windows/view/view_interface.h
+++ dev/View/Desktop/Interfaces/Windows/view/view_interface.h
@@ -2,13 +2,13 @@
 #define VIEW_INTERFACE_PROC_H
 
 #include "../../system_interface.h"
-#ifdef WIN32
+#ifdef IS_WIN32
 
 #include <Windows.h>
 
-
+extern HINSTANCE window_hInstance;
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-#endif // WIN32
+#endif // IS_WIN32
 
 #endif // !VIEW_INTERFACE_PROC_H
