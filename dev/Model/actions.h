21b4292250f855bd9f73497e3a7490658cead24e
smileyface <kasonbennett65@gmail.com>
2020-12-13 16:08:07 -0700

    INITAIL NODE

--- dev/Model/actions.h
+++
@@ -1,7 +0,0 @@
-#ifndef MODEL_ACTIONS_H
-#define MODEL_ACTIONS_H
-
-#include "devices/actions/device.h"
-#include "devices/actions/gradient.h"
-#include "devices/actions/switch.h"
-#endif // !MODEL_ACTIONS_H
