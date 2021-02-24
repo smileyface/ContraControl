4e4a2d675425bcc211571eb835fbafa49c2b8e7f
smileyface <kasonbennett65@gmail.com>
2021-02-23 18:27:03 -0700

    Move testing to a non-reserved folder name

---
+++ unit_test/tests/Controller.cpp
@@ -0,0 +1,15 @@
+#include "../utilities/device_utilities.h"
+#include "../utilities/system_testings.h"
+
+#include "../utilities/pch.h"
+
+TEST(Controller_Tests, Gradient_Initalize) {
+	std::string device = device_utilities::add_device(new Gradient_Device());
+	device_state ds;
+	ds.initalized = true;
+	ds.valid = true;
+
+	device_utilities::check_state(device, ds);
+
+	system_util::cleanup();
+}
