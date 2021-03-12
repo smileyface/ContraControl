f80883df29deda99da8048798ccc56b937291f8b
smileyface <kasonbennett65@gmail.com>
2021-03-12 15:31:01 -0700

    Use Device_Label which includes Node_Id Device creation happens at a Node level

--- dev/Model/device.h
+++ dev/Model/device.h
@@ -8,7 +8,7 @@
 #include "devices/gradient.h"
 #include "devices/channel.h"
 
-enum class DEVICE_IDENTIFIER
+enum class DEVICE_IDENTIFIER : uint16_t
 {
 	DEVICE,
 	SWITCH,
@@ -16,5 +16,20 @@ enum class DEVICE_IDENTIFIER
     CHANNEL,
 	AUDIO
 };
+
+inline Device* create_device_instance(Device_Creator creator)
+{
+	Device* the_device;
+	switch ((DEVICE_IDENTIFIER)creator.first)
+	{
+	case DEVICE_IDENTIFIER::SWITCH:
+		the_device = new Switch_Device();
+		break;
+	default:
+		the_device = new Device();
+	}
+	the_device->set_name(creator.second);
+	return the_device;
+}
 
 #endif // !DEVICES_H
