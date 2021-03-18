81f816d5594f784d5b1131bde9cb550b9e3bfb59
smileyface <kasonbennett65@gmail.com>
2021-03-18 13:05:51 -0700

    Testing setup more robust

--- unit_test/tests/Switch_Device.cpp
+++ unit_test/tests/Switch_Device.cpp
@@ -7,8 +7,27 @@
 
 namespace {
 	class Switch_Commands_Test : public ::testing::Test {
+	protected:
+		Device_Label dl;
 		virtual void SetUp() {
 			system_util::setup();
+			device_utilities::create_node("Test_Node_1");
+			dl = device_utilities::add_device("Test_Node_1", Device_Creator((int)DEVICE_IDENTIFIER::SWITCH, "Test1"));
+			device_utilities::initalize_device(dl);
+		}
+		virtual void TearDown() {
+			system_util::cleanup();
+		}
+
+	};
+
+	class Switch_Commands_Test_No_Init : public ::testing::Test {
+	protected:
+		Device_Label dl;
+		virtual void SetUp() {
+			system_util::setup();
+			device_utilities::create_node("Test_Node_1");
+			dl = device_utilities::add_device("Test_Node_1", Device_Creator((int)DEVICE_IDENTIFIER::SWITCH, "Test1"));
 		}
 		virtual void TearDown() {
 			system_util::cleanup();
@@ -18,9 +37,6 @@ namespace {
 }
 
 TEST_F(Switch_Commands_Test, Switch_Initalize) {
-	device_utilities::create_node("Test_Node_1");
-	Device_Label dl = device_utilities::add_device("Test_Node_1", Device_Creator((int)DEVICE_IDENTIFIER::SWITCH, "Test1"));
-	device_utilities::initalize_device(dl);
 	Device_State ds;
 	ds.initalized = true;
 	ds.valid = true;
@@ -29,20 +45,12 @@ TEST_F(Switch_Commands_Test, Switch_Initalize) {
 }
 
 TEST_F(Switch_Commands_Test, Switch_On) {
-	device_utilities::create_node("Test_Node_1");
-	Device_Label dl = device_utilities::add_device("Test_Node_1", Device_Creator((int)DEVICE_IDENTIFIER::SWITCH, "Test1"));
-	device_utilities::initalize_device(dl);
-
 	Device_State ds = device_utilities::command_device(dl, new On());
 	testing_util::device_utilities::check_state(dl, ds);
 	system_util::cleanup();
 }
 
 TEST_F(Switch_Commands_Test, Switch_Off) {
-	device_utilities::create_node("Test_Node_1");
-	Device_Label dl = device_utilities::add_device("Test_Node_1", Device_Creator((int)DEVICE_IDENTIFIER::SWITCH, "Test1"));
-	device_utilities::initalize_device(dl);
-
 	Device_State ds = device_utilities::command_device(dl, new On());
 	testing_util::device_utilities::check_state(dl, ds);
 
@@ -53,10 +61,7 @@ TEST_F(Switch_Commands_Test, Switch_Off) {
 
 }
 
-TEST_F(Switch_Commands_Test, Switch_Invalid) {
-	device_utilities::create_node("Test_Node_1");
-	Device_Label dl = device_utilities::add_device("Test_Node_1", Device_Creator((int)DEVICE_IDENTIFIER::SWITCH, "Test1"));
-
+TEST_F(Switch_Commands_Test_No_Init, Switch_Invalid) {
 	device_utilities::command_device(dl, new Transition(0, 0));
 	testing_util::device_utilities::check_validity(dl, false);
 
