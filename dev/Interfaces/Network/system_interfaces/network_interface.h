0f51367791c00de6be7e55f8599952602171fa2c
smileyface <kason.bennett@aggiemail.usu.edu>
2021-10-03 17:01:14 +0000

    Network Interface

--- dev/Interfaces/Network/system_interfaces/network_interface.h
+++ dev/Interfaces/Network/system_interfaces/network_interface.h
@@ -9,36 +9,109 @@
 const unsigned short DEFAULT_PORT = 0xDCF5;
 const std::string invalid_hostname = "INVALID";
 
+/**
+ * \brief Interface for handling network communication between Nodes.
+ * 
+ */
 class Network_Interface
 {
 public:
-    void set_server();
-    void set_client();
-    virtual void initalized() = 0;
+    /**
+     * \brief Initalize the interface.
+     *
+     * Creates the socket.
+     * 
+     * \return This should not return anything, but Doxygen thinks it does.
+     */
     virtual void initalize() = 0;
+    /**
+     * \brief Check if the interface is initalized.
+     * 
+     * If the interface is not initalized, this sets the status_state as an error. 
+     * If it is initalized, it sets the state as NETWORK_STATUS::NETWORK_INITALIZED.
+     * \return This should not return anything, but Doxygen thinks it does.
+     */
+    virtual void initalized() = 0;
+    /**
+     * \brief Close all interface objects
+     * \return This should not return anything, but Doxygen thinks it does.
+     */
     virtual void clean_up() = 0;
-    Network_Status_State get_status();
 
     //getters
+    /**
+     * \brief Get the status_state
+     * 
+     * \return Network_Status_State 
+     */
+    Network_Status_State get_status();
+    /**
+     * \brief Return the IP of the local node
+     * 
+     * \return ipv4 as 4 unsigned chars
+     */
     unsigned char* local_ip();
 
     //setters
+        /**
+     * \brief Set the Node as server.
+     */
+    void set_server();
+    /**
+     * \brief Set the Node as client.
+     */
+    void set_client();
+    /**
+     * \brief Set the my ip object. Does it from the ipconfig/ifconfig
+     *
+     * \return This should not return anything, but Doxygen thinks it does.
+     */
     virtual void set_my_ip() = 0;
 
     //Client Code
+    /**
+     * \brief Connect to a server.
+     * 
+     * \param addr Address of server.
+     * \return This should not return anything, but Doxygen thinks it does.
+     */
     virtual void connect_to_server(ipv4_addr addr) = 0;
+    /**
+     * \brief Scan for a server to connect to.
+     * 
+     * \todo Do this through broadcast instead of an ARP blast.
+     * \return This should not return anything, but Doxygen thinks it does.
+     */
     virtual void scan_for_server() = 0;
     
     //Server Code
+    /**
+     * \brief Start the server in a thread
+     * \return This should not return anything, but Doxygen thinks it does.
+     */
     virtual void server_start() = 0;
 
    //Constants
+   /**
+    * \brief localhost address
+    */
     const ipv4_addr localhost = ipv4_addr("127.0.0.1");
 protected:
+    /**
+     * \brief Is the local node a server.
+     */
     bool is_server = false;
+    /**
+     * \brief The local node ip address.
+     */
     ipv4_addr my_ip = ipv4_addr("255.255.255.255");
+    /**
+     * \brief the interface name of the local node.
+     */
     std::string hostname;
-
+    /**
+     * \brief State of the network interface.
+     */
     Network_Status_State status_state;
 };
 
