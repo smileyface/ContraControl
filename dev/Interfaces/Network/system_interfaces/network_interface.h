8c4ceaa48fa60e9ad6c57317790b578daa1f2e9a
smileyface <kasonbennett65@gmail.com>
2021-10-03 10:41:35 -0700

    The virtual return type issue seems to only be a thing on linux.

--- dev/Interfaces/Network/system_interfaces/network_interface.h
+++ dev/Interfaces/Network/system_interfaces/network_interface.h
@@ -20,8 +20,6 @@ public:
      * \brief Initalize the interface.
      *
      * Creates the socket.
-     * 
-     * \return This should not return anything, but Doxygen thinks it does.
      */
     virtual void initalize() = 0;
     /**
@@ -29,12 +27,10 @@ public:
      * 
      * If the interface is not initalized, this sets the status_state as an error. 
      * If it is initalized, it sets the state as NETWORK_STATUS::NETWORK_INITALIZED.
-     * \return This should not return anything, but Doxygen thinks it does.
      */
     virtual void initalized() = 0;
     /**
-     * \brief Close all interface objects
-     * \return This should not return anything, but Doxygen thinks it does.
+     * \brief Close all interface objects.
      */
     virtual void clean_up() = 0;
 
@@ -63,8 +59,6 @@ public:
     void set_client();
     /**
      * \brief Set the my ip object. Does it from the ipconfig/ifconfig
-     *
-     * \return This should not return anything, but Doxygen thinks it does.
      */
     virtual void set_my_ip() = 0;
 
@@ -73,21 +67,18 @@ public:
      * \brief Connect to a server.
      * 
      * \param addr Address of server.
-     * \return This should not return anything, but Doxygen thinks it does.
      */
     virtual void connect_to_server(ipv4_addr addr) = 0;
     /**
      * \brief Scan for a server to connect to.
      * 
      * \todo Do this through broadcast instead of an ARP blast.
-     * \return This should not return anything, but Doxygen thinks it does.
      */
     virtual void scan_for_server() = 0;
     
     //Server Code
     /**
      * \brief Start the server in a thread
-     * \return This should not return anything, but Doxygen thinks it does.
      */
     virtual void server_start() = 0;
 
