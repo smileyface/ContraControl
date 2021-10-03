c2aec2adc742292e536515ba657779d322bfd453
smileyface <kason.bennett@aggiemail.usu.edu>
2021-10-03 18:07:23 +0000

    Network interface documentation

--- dev/Interfaces/Network/system_interfaces/network_interface.h
+++ dev/Interfaces/Network/system_interfaces/network_interface.h
@@ -106,15 +106,36 @@ protected:
     Network_Status_State status_state;
 };
 
+/**
+ * \brief Main network interface. Use this instead of creating a new Network_Interface object.
+ * 
+ */
 namespace network
 {
-    extern Network_Interface* network_interface;
+    extern Network_Interface* network_interface; ///<Main interface object.
 
+    /**
+     * \brief Setup network interface as a system specific interface.
+     */
     extern void init_network_interfaces();
+    /**
+     * \brief Teardown and clean up system specific network interface.
+     */
     extern void teardown_network_interfaces();
-    extern void send_message(MESSAGE*);
+    /**
+     * \brief Send a message on the network interface.
+     * 
+     * \param mesg Message to send
+     */
+    extern void send_message(MESSAGE* mesg);
 
+    /**
+     * \brief Start interface as a server.
+     */
     extern void start_server();
+    /**
+     * \brief Start interface as a client.
+     */
     extern void start_client();
 }
 
