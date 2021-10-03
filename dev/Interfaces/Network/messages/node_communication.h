aa1827e9fa6d276c683a681d648b388f58afa5b6
smileyface <kasonbennett65@gmail.com>
2021-10-03 07:49:49 -0700

    Message Documentation.

--- dev/Interfaces/Network/messages/node_communication.h
+++ dev/Interfaces/Network/messages/node_communication.h
@@ -2,10 +2,22 @@
 
 namespace Node_Messages
 {
+	/**
+	 * Message letting the network know the local node has started.
+	 * |      |      |
+	 * |:----:|:----:|
+	 * |0-16  | 17-n |
+	 * |ipv4  | name |
+	 */
 	struct NODE_HELLO : MESSAGE
 	{
-		unsigned char addr[4];
-		Message_String id;
+		unsigned char addr[4]; ///<The ip address of the node
+		Message_String id; ///<The name of the node
+		/**
+		 * Constructor
+		 * \param in_addr The ip address of the node.
+		 * \param in_id The human readable name of the node.
+		 */
 		NODE_HELLO(unsigned char* in_addr, const char* in_id);
 	};
 }
