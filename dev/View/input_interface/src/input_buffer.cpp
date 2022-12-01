5e7e0bd0a8248b3547f9963832456dbb14714c4f
smileyface <kasonbennett65@gmail.com>
2022-12-01 09:32:58 -0700

    coverage

--- dev/View/input_interface/src/input_buffer.cpp
+++ dev/View/input_interface/src/input_buffer.cpp
@@ -14,9 +14,9 @@ char KPI_to_ascii(std::pair<KPI, KEY_STATE> key)
 {
     char ascii_chart_conversion = 0;
     char val = 0;
-    if(key.second == KEY_STATE::PRESSED)
+    if(key.first >= KEY::A && key.first <= KEY::Z)
     {
-        if(key.first >= KEY::A && key.first <= KEY::Z)
+        if(key.second == KEY_STATE::PRESSED)
         {
             ascii_chart_conversion = 64;
             if(!shifted)
@@ -24,22 +24,22 @@ char KPI_to_ascii(std::pair<KPI, KEY_STATE> key)
                 ascii_chart_conversion += 32;
             }
         }
-        else if(key.first >= KEY::NUM_0 && key.first <= KEY::NUM_9)
-        {
-            ascii_chart_conversion = 48 - KEY::NUM_0.index();
-        }
-        else if(key.first >= KEY::NUM_PAD::NUM_0 && key.first <= KEY::NUM_PAD::NUM_9)
-        {
-            ascii_chart_conversion = 48 - KEY::NUM_PAD::NUM_0.index();
-        }
-        else if(key.first == KEY::L_SHIFT)
-        {
-            shifted = true;
-        }
     }
-    else if(key.second == KEY_STATE::RELEASED)
+    else if(key.first >= KEY::NUM_0 && key.first <= KEY::NUM_9)
     {
-        if(key.first == KEY::L_SHIFT)
+        if(key.second == KEY_STATE::PRESSED)
+            ascii_chart_conversion = 48 - KEY::NUM_0.index();
+    }
+    else if(key.first >= KEY::NUM_PAD::NUM_0 && key.first <= KEY::NUM_PAD::NUM_9)
+    {
+        if(key.second == KEY_STATE::PRESSED)
+            ascii_chart_conversion = 48 - KEY::NUM_PAD::NUM_0.index();
+    }
+    else if(key.first == KEY::L_SHIFT)
+    {
+        if(key.second == KEY_STATE::PRESSED)
+            shifted = true;
+        else if(key.second == KEY_STATE::RELEASED)
             shifted = false;
     }
     if(ascii_chart_conversion > 0)
