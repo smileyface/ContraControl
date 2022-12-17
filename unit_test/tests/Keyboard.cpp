36e13c824ded5e7ac642b6f6855f93d87759a702
smileyface <kasonbennett65@gmail.com>
2022-12-16 19:01:19 -0700

    CI debugging

--- unit_test/tests/Keyboard.cpp
+++ unit_test/tests/Keyboard.cpp
@@ -13,13 +13,14 @@ namespace
 
 		system_utilities::keyboard_utilities::Keyboard keyboard;
 		Message_Consumer* consumer = 0;
-		virtual void SetUp()
+		virtual void SetUp() override
 		{
 			keyboard.get_interface()->connect_to_keyboard();
 			keyboard.get_interface()->start_listening();
 
 			if(system_utilities::CI && system_utilities::LINUX)
 			{
+				LOG_INFO("Skipping hardware interface test on Linux CI", "Keyboard test setup");
 				GTEST_SKIP() << "Github Actions has input permission issues on Linux, so I'm not running those tests right now.\nPlease make sure you've run these tests locally before merging the PR.";
 			}
 		}
