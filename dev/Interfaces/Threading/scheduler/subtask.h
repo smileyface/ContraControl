4c6d1250550d67f0c1defb9b210fb6d340b73e5d
smileyface <kasonbennett65@gmail.com>
2023-07-17 13:29:43 -0700

    Documentation update

--- dev/Interfaces/Threading/scheduler/subtask.h
+++ dev/Interfaces/Threading/scheduler/subtask.h
@@ -21,11 +21,13 @@ struct Subtask
 /**
  * @brief A subtask that is removed from the run list after it is run. A run-once task.
  * @param subtask A subtask to run as a one off
+ * @return A created one off sub task.
  */
 Subtask Cleaned_Task(std::function<void()> subtask);
 
 /**
  * @brief A subtask that sticks around the run list after it is run.
  * @param subtask A subtask to run everytime the task is run.
+ * @return A created sub task that will run everytime the parent task is run.
  */
 Subtask Sticky_Task(std::function<void()> subtask);
