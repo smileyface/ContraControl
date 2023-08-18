ac6e5903b6c7c29f1eae6d1c0f1e25103f377687
smileyface <kasonbennett65@gmail.com>
2023-08-17 20:16:06 -0700

    Clang satisfying

--- dev/Interfaces/Interfaces/src/packed_command.cpp
+++ dev/Interfaces/Interfaces/src/packed_command.cpp
@@ -4,13 +4,20 @@
 
 Packed_Command::Packed_Command() :
 	command(0),
-	device_label(Device_Label()),
 	time(0.0),
 	run(false),
 	sent(false)
 { }
 
 Packed_Command::Packed_Command(const Packed_Command& cmd) :
+	command(cmd.command),
+	device_label(cmd.device_label),
+	time(cmd.time),
+	run(cmd.run),
+	sent(cmd.sent)
+{ }
+
+Packed_Command::Packed_Command(const Packed_Command&& cmd) :
 	command(cmd.command),
 	device_label(cmd.device_label),
 	time(cmd.time),
@@ -26,25 +33,22 @@ Packed_Command::Packed_Command(Command* cmd, Device_Label label, double time) :
 	sent(false)
 { }
 
-Packed_Command::Packed_Command(Command* cmd, Device_Label label) :
-	command(cmd),
-	device_label(std::move(label)),
-	time(0.0),
-	run(false),
-	sent(false)
-{ }
-
 Packed_Command::~Packed_Command()
 { }
 
-
-
-bool Packed_Command::operator<(const Packed_Command& cmd) const
+Packed_Command& Packed_Command::operator=(const Packed_Command& cmd)
 {
-	return time < cmd.time;
+	if(this != &cmd)
+	{
+		this->command = cmd.command;
+		this->device_label = cmd.device_label;
+		this->time = cmd.time;
+		this->run = cmd.run;
+	}
+	return *this;
 }
 
-Packed_Command& Packed_Command::operator=(const Packed_Command& cmd)
+Packed_Command& Packed_Command::operator=(const Packed_Command&& cmd)
 {
 	if(this != &cmd)
 	{
--- dev/Interfaces/Interfaces/types/packed_command.h
+++ dev/Interfaces/Interfaces/types/packed_command.h
@@ -26,29 +26,26 @@ public:
 	 */
 	Packed_Command(const Packed_Command& cmd);
 	/**
+	 * Move Constructor.
+	 * @param cmd Packed command to copy.
+	 */
+	Packed_Command(const Packed_Command&& cmd);
+	/**
 	 @param cmd Command to run.
 	 @param label Label that maps to the device to command.
 	 @param time Time from start to run the command.
 	 */
 	Packed_Command(Command* cmd, Device_Label label, double time);
-	/**.
-	* @brief Instantaneous command. Will run on the next frame
-	@param cmd Command to run.
-	@param label Label that maps to the device to command.
-	@param t Time from start to run the command.
-	*/
-	Packed_Command(Command* cmd, Device_Label label);
-	/**
-	 @param tc Timed_Command to compare to.
-	 @return If tc happens after this command.
-	 */
-	bool operator<(const Packed_Command& cmd) const;
-
 	/**
 	* Assignment operator.
 	 * @param tc Timed_Command to copy to.
 	 */
 	Packed_Command& operator=(const Packed_Command& cmd);
+	/**
+	* Move Assignment operator.
+	 * @param tc Timed_Command to copy to.
+	 */
+	Packed_Command& operator=(const Packed_Command&& cmd);
 
 	/**
 	 Command to run
