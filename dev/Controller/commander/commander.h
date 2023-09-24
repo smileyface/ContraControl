b681f82e2602cb4b90db0dc08a279f7d91def2d7
smileyface <kasonbennett65@gmail.com>
2023-09-24 08:50:05 -0700

    Document updating

--- dev/Controller/commander/commander.h
+++ dev/Controller/commander/commander.h
@@ -10,12 +10,29 @@
 #include <stdarg.h>     /* va_list, va_start, va_arg, va_end */
 
 #include "Interfaces/types/packed_command.h"
+
+/**
+ * An interface for Command memory management.
+ */
 class Commander
 {
 public:
+	/**
+	 * Singleton get instance
+	 * \return Singleton instance
+	 */
 	static Commander* get_instance();
+	/**
+	 * Create a new command and put it on the command_list
+	 * \tparam T Type of Command to create
+	 * \param a Argument list for Command construction.
+	 * \return A pointer to the newly created Command.
+	 */
 	template<typename T, typename... Args>
 	Command* make_command(Args&&... a);
+	/**
+	 * Destruct and remove all Commands on the command_list that are marked as completed.
+	 */
 	void clean_list();
 private:
 	std::vector<Command*> command_list;
